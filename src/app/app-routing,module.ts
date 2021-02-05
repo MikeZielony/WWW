8ddf87c9ef024c6a4c770d5531ffc748426199b6
mikezielony <mike.zielony@gmail.com>
2021-02-05 07:37:02 +0100

    Add routing

--- src/app/app-routing,module.ts
+++ src/app/app-routing,module.ts
@@ -7,16 +7,18 @@ import {KontaktComponent} from './kontakt/kontakt.component';
 import {MapaComponent} from './mapa/mapa.component';
 import {NavbarComponent} from './navbar/navbar.component';
 import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
+import {MenuComponent} from './menu/menu.component';
 
 
 const routes: Routes = [
-  {path: '', redirectTo: '/', pathMatch: 'full'},
+  {path: '', redirectTo: '/menu', pathMatch: 'full'},
   {path: 'blog', component: BlogComponent},
   {path: 'home', component: HomeComponent},
   {path: 'actual', component: AktualComponent},
   {path: 'kontakt', component: KontaktComponent},
   {path: 'mapa', component: MapaComponent},
   {path: 'navbar', component: NavbarComponent},
+  {path: 'menu', component: MenuComponent},
   { path: '*', component: PageNotFoundComponent },
 ];
 
