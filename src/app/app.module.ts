766dae4a981a91d8309d7f98812c98fb9177a1ac
NataliaRajska <natalia.rajskaa@gmail.com>
2021-02-09 17:36:19 +0100

    Add providers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
-import { MenuComponent } from './menu/menu.component';
-import { HomeComponent } from './home/home.component';
-import { AktualComponent } from './aktual/aktual.component';
-import { BlogComponent } from './blog/blog.component';
-import { KontaktComponent } from './kontakt/kontakt.component';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {AppComponent} from './app.component';
+import {MenuComponent} from './menu/menu.component';
+import {HomeComponent} from './home/home.component';
+import {AktualComponent} from './aktual/aktual.component';
+import {BlogComponent} from './blog/blog.component';
+import {KontaktComponent} from './kontakt/kontakt.component';
 import {AppRoutingModule} from './app-routing,module';
 import {HttpClientModule} from '@angular/common/http';
-import { MapComponent } from './mapa/map.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import {MapComponent} from './mapa/map.component';
+import {NavbarComponent} from './navbar/navbar.component';
+import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
+import {AngularFireModule} from "@angular/fire";
+import {
+  GoogleLoginProvider,
+  SocialAuthServiceConfig,
+  SocialLoginModule
+} from "angularx-social-login";
 
 @NgModule({
   declarations: [
@@ -23,12 +28,41 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
     KontaktComponent,
     MapComponent,
     NavbarComponent,
-    PageNotFoundComponent
+    PageNotFoundComponent,
   ],
   imports: [
-    BrowserModule, AppRoutingModule, HttpClientModule,
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    SocialLoginModule,
+    AngularFireModule.initializeApp(
+      {
+        apiKey: 'AIzaSyC0hoicvjhmcmk_swt69z4eYs7JhmOyuKM',
+        authDomain: 'travel-app-ba3a2.firebaseapp.com',
+        databaseURL: 'https://travel-app-ba3a2.firebaseio.com',
+        projectId: 'travel-app-ba3a2',
+        storageBucket: 'travel-app-ba3a2.appspot.com',
+        messagingSenderId: '725560342179',
+        appId: '1:725560342179:web:9d2ca913c359c5b90d7116',
+        measurementId: 'G-12EXKLWETH'
+      }
+    )
   ],
-  providers: [],
+  providers: [{
+    provide: 'SocialAuthServiceConfig',
+    useValue: {
+      autoLogin: false,
+      providers: [
+        {
+          id: GoogleLoginProvider.PROVIDER_ID,
+          provider: new GoogleLoginProvider(
+            '725560342179-cim8ajd2b3ioted4srql213jg1j4nkfa.apps.googleusercontent.com'
+          )
+        }
+      ]
+    } as SocialAuthServiceConfig,
+  }],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
